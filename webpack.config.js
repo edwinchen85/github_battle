c1fa15005b20fcda8a1a586506e0e7cce52421ec
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 07:40:34 +0800

    Set devServer to reroute to localhost:8080 first before setting the publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
+  devServer: {
+    historyApiFallback: true
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: 'app/index.html'
