25f3f9c7442ca748277cf430a292889277feab1a
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 05:37:23 +0800

    Add componentDidMount() life cycle for <Popular />

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -38,12 +38,26 @@ class Popular extends React.Component {
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function() {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
     });
+
+    api.fetchPopularRepos(this.state.selectedLanguage)
+      .then(function(repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          }
+        });
+      }.bind(this));
   }
 
   render() {
