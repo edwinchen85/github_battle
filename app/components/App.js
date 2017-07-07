b70200f07ede92f7fe015482af836aa36c9a055d
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 07:46:01 +0800

    Import and render Switch component that will render 404 page if page is not found

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
+var Switch = ReactRouter.Switch;
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Nav = require('./Nav');
@@ -13,9 +14,14 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/battle" component={Battle} />
-          <Route path="/popular" component={Popular} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
         </div>
       </Router>
     )
