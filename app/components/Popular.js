940863ef4af467887322ebe5d2a68446a5d3582d
edwinchen85 <edwinchen85@gmail.com>
2017-07-07 16:16:02 +0800

    Add languages array

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,8 @@ var React = require('react');
 
 class Popular extends React.Component {
   render() {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
       <div>Popular!</div>
     )
