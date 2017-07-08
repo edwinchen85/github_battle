f762f4d8659a7c5d8b50700b325491e7ae81bf1d
Edwin Chen <edwinchen85@gmail.com>
2017-07-09 01:01:51 +0800

    Parse ‘search string’ using query-string

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,11 @@
 var React = require('react');
+var queryString = require('query-string');
 
 class Results extends React.Component {
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+  }
+
   render() {
     return (
       <div>Results</div>
