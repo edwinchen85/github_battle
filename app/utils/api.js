87d24ff6e0463c1de8a433ae6e48e7c2d94964f1
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 21:11:14 +0800

    Define getRepos() method

--- app/utils/api.js
+++ app/utils/api.js
@@ -11,6 +11,10 @@ function getProfile(username) {
     })
 }
 
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+}
+
 module.exports = {
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
