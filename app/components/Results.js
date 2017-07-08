8fb528c5837f82da7714b0909f75408cbe55db92
Edwin Chen <edwinchen85@gmail.com>
2017-07-09 01:10:11 +0800

    Invoke api.battle() method to get battle results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,33 @@
 var React = require('react');
 var queryString = require('query-string');
+var api = require('../utils/api');
 
 class Results extends React.Component {
   componentDidMount() {
     var players = queryString.parse(this.props.location.search);
+
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function(results) {
+      if(results === null) {
+        return this.setState(function() {
+          return {
+            error: 'Looks like there was error. Check that both users exist on Github',
+            loading: false
+          };
+        });
+      }
+
+      this.setState(function() {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      });
+    }.bind(this));
   }
 
   render() {
