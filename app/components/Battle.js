9fe1d27c4cc99bfed4b796cc203d7d9a02d3f1fe
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 09:16:57 +0800

    Add constructor for <Battle />

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,17 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+  }
+
   render() {
     return (
       <div>
