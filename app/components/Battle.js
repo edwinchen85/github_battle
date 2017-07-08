da91cec1a93dbc794a3b4507c1986ec026295512
Edwin Chen <edwinchen85@gmail.com>
2017-07-08 09:20:16 +0800

    Add handleSubmit() method of <Battle />

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -10,6 +10,17 @@ class Battle extends React.Component {
       playerOneImage: null,
       playerTwoImage: null
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function() {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    });
   }
 
   render() {
